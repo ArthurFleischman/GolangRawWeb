2d45433a152a1823869b259c81375aa6b27a0300
ArthurFleischman <cabralfle@gmail.com>
2020-05-09 21:24:09 -0300

    adding css

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/TKfleBR/GolangRawWeb
+
+go 1.14
+
+require github.com/satori/go.uuid v1.2.0
--- main.go
+++ main.go
@@ -25,11 +25,11 @@ var tmp *template.Template
 
 func init() {
 	tmp = template.Must(template.ParseGlob("templates/*.html"))
+
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	tmp.ExecuteTemplate(w, "index.html", nil)
-
 }
 
 func signup(w http.ResponseWriter, r *http.Request) {
@@ -38,12 +38,8 @@ func signup(w http.ResponseWriter, r *http.Request) {
 		pass := r.FormValue("password")
 		u := user{Name: name, password: pass}
 		users[name] = u
-		/* ==============================writing=log==============================*/
-		f, _ := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		defer f.Close()
-		log.SetOutput(f)
-		log.Printf("user %s registered\n", u.Name)
-		/* ==============================writing=log==============================*/
+		logMessage := fmt.Sprintf("user %s registered\n", u.Name)
+		log.Printf(logMessage)
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 	tmp.ExecuteTemplate(w, "signup.html", nil)
@@ -57,12 +53,8 @@ func isLogged(r *http.Request) bool {
 		fmt.Println("ok")
 		return true
 	} else {
-		/* ==============================writing=log==============================*/
-		f, _ := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		defer f.Close()
-		log.SetOutput(f)
-		log.Printf("invalid cookie %s\n", c.Value)
-		/* ==============================writing=log==============================*/
+		logMessage := fmt.Sprintf("invalid cookie %s\n", c.Value)
+		log.Printf(logMessage)
 		return false
 	}
 }
@@ -77,32 +69,17 @@ func login(w http.ResponseWriter, r *http.Request) {
 		name := r.FormValue("username")
 		pass := r.FormValue("password")
 		if users[name].Name == name && users[pass].password == pass {
-			id, err := uuid.NewV4()
-			if err != nil {
-				/* ==============================writing=log==============================*/
-				f, _ := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-				defer f.Close()
-				log.SetOutput(f)
-				log.Println("error", err)
-				/* ==============================writing=log==============================*/
-			} else {
-				c := &http.Cookie{
-					Name:  "session",
-					Value: id.String(),
-				}
-				http.SetCookie(w, c)
-				sessions[c.Value] = name
-
+			id := uuid.NewV4()
+			c := &http.Cookie{
+				Name:  "session",
+				Value: id.String(),
 			}
+			http.SetCookie(w, c)
+			sessions[c.Value] = name
+
 			http.Redirect(w, r, "/home", http.StatusSeeOther)
 
 		} else {
-			/* ==============================writing=log==============================*/
-			f, _ := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-			defer f.Close()
-			log.SetOutput(f)
-			log.Printf("failed to log as %s\n", users[name].Name)
-			/* ==============================writing=log==============================*/
 
 		}
 	}
@@ -112,9 +89,16 @@ func home(w http.ResponseWriter, r *http.Request) {
 	c, _ := r.Cookie("session")
 	tmp.ExecuteTemplate(w, "home.html", sessions[c.Value])
 }
+func myLog(message string) {
+	/* ==============================writing=log==============================*/
+	f, _ := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	defer f.Close()
+	log.SetOutput(f)
+	log.Printf(message)
+}
 
 func main() {
-
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 	http.HandleFunc("/", index)
 	http.HandleFunc("/signup", signup)
 	http.HandleFunc("/login", login)
